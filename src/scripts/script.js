295c5fa8e925a42a473e5f7f6ebeb37a224d5288
plastm0oo <maxihjn@yandex.ru>
2024-10-15 17:17:15 +0300

    workshop3

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,25 @@
+const modal = document.querySelector('.modal')
+//console.log(modal);
+let isModalOpen = false;
 
+function toggleModal() {
+    if (isModalOpen == true) {
+        modal.style.display = 'none';
+        isModalOpen = false;
+    }
+    else {
+        modal.style.display = 'flex';
+        isModalOpen = true;
+    }
+}
+
+const form = document.querySelector('.modal__form')
+
+form.addEventListener('submit', (event)=>{ //стрелочная функция
+    event.preventDefault()
+    const inputValues = Array.from(document.querySelectorAll('.modal__form-input')).map((input)=>{
+        return input.value
+    })
+    console.log(inputValues)
+    form.reset()
+})
